552e85667fe8a860230cf0a2d8d19554400e2591
Helen <nikishina.olena@gmail.com>
2018-01-13 03:39:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function theBeatlesPlay(musicians,instruments){
+  
+}
+
+function johnLennonFacts(array){
+  const facts = ["!!!"];
+}
+
+function iLoveTheBeatles(array){
+  return array("I love the Beatles!")
+}

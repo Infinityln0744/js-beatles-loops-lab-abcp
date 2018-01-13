ce13191825715e8467235dabe4199fc45f3d2391
Helen <nikishina.olena@gmail.com>
2018-01-13 03:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function theBeatlesPlay(musicians,instruments){
-  
+function theBeatlesPlay(musicians, instruments) {
+var array = [];
+ for (var i = 0; i < musicians.length; i++) {
+	array.push(musicians[i] + ' plays ' + instruments[i]);
+}
+return array;
 }
 
-function johnLennonFacts(array){
-  const facts = ["!!!"];
+function johnLennonFacts(facts) {
+var results = [];
+ var count = 0;
+ while (count < facts.length) {
+  results.push(facts[count] + "!!!");
+  count++;
+}
+ return results;
 }
 
-function iLoveTheBeatles(array){
-  return array("I love the Beatles!")
-}
+ function iLoveTheBeatles(num) {
+ var array = [];
+  do {
+    array.push("I love the Beatles!");
+  num++;
+} while (num < 15);
+return array;
+ }
